54d87f8673474e67ae50e5ddd1bd21828569acfe
Deepak Nagarkoti <nagarkotideepak9843@gmail.com>
2022-04-18 11:43:58 +0545

    Add lession 1 and refactoring

---
+++ classes/CSharpFundamentals/LessonA1-DataTypes.cs
@@ -0,0 +1,45 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+namespace ABC{
+    class EntryPoint{
+        public static void Main(){
+            //This is  single line comment
+            /* 
+            This is the multiple line
+            comment.
+            */
+            // Pascal Casing: method and class 
+            // Camel Casing: abcDef --> Varibales
+            // Methods == Functions  --->  Pascal Casing
+
+            Console.Write("Hey There!!");  
+            // Data Types Direct Vs Implicit
+            // Numbers --> Integer, Floating point
+            byte a=24// 8-bit
+            short b=334;  // 16-bit 
+            int x = 553231;
+            long d=111122222222223333;
+
+            float e = 467.232f;
+            double f = 2321.32444444;
+            decimal g=234444.2344444444444444444444444; //16-bit
+            
+            bool h=true;
+
+            char i = 'D';
+            string j = "Deep";
+
+            object 0 = "";
+
+            // Nullable 
+            int? population = 2342;
+
+            //Var type --> Dynamic value assigning
+            var x = GetSum();
+
+
+
+
+        }
+    }
+}
