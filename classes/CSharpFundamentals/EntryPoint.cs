8bd211e3e861a30cae7b4b3c83940700d24e04a1
Deepak Nagarkoti <nagarkotideepak9843@gmail.com>
2022-04-19 12:04:39 +0545

    Learn Methods syntax

---
+++ classes/CSharpFundamentals/EntryPoint.cs
@@ -0,0 +1,8 @@
+Console.Write("Hey There!");
+
+Methods m1 = new();
+var s = m1.Sum();
+
+s = m1.Sum(23,4,56.7);
+s = m1.Sum(34.2);
+s = m1.Sum(2.3, 4.6, 12.4);
--- classes/CSharpFundamentals/LessonC3-Methods.cs
+++ classes/CSharpFundamentals/LessonC3-Methods.cs
@@ -1,21 +1,30 @@
 class Methods
 {
+    // 1. No return value, no parameter
     void PrintHello()
     {
         Console.WriteLine("Hello Bouy!!");
     }
 
+    // 2. No return value but single parameter
     void PrintHello(string name)
     {
         Console.WriteLine("Hello, " + name ); //Concatenation
         Console.WriteLine($"Hello, {name}" ); //Dynamic Variable use
     }
 
-    string FormatName(string firstNamme, string lastName)
+    // 3. return value and two parameter
+    string FormatName(string firstName, string lastName)
     {
         return $"{lastName}, {firstName}";
     }
 
-    //
+    // 4. Expression Syntax
+    string FormatName2(string firstName, string lastName) => $"{lastName}, {firstName}" ;
+    // Method Expression Syntax
+    bool IsEven(short number) => number % 2 == 0;
+
+    // 5. Variable number of arguments-->  internal vs public access modifier
+    public double Sum(params double[] numbers) => numbers.Sum();    
 
 }
