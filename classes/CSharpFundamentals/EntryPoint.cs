c3fb5a462a48c28c9453cab9c9b98707436cb7c6
Deepak Nagarkoti <nagarkotideepak9843@gmail.com>
2022-05-04 12:11:04 +0545

    .

--- classes/CSharpFundamentals/EntryPoint.cs
+++ classes/CSharpFundamentals/EntryPoint.cs
@@ -31,9 +31,16 @@
 
 public class Program
 {
+    public static void Main(){
     Person p1 = new();
     p2.Name = "c";
     var x = p1.Name;
     var y = p1.Address;
 
+    // Defining Constants Two ways
+    const  int f = 445;
+    readonly byte g = 45;
+
+
+    }
 }
