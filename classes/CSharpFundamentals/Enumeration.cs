da09a78b1a56acf075f82ac6af352426d2b37c40
Deepak Nagarkoti <nagarkotideepak9843@gmail.com>
2022-05-04 12:07:59 +0545

    Learn About Properties

---
+++ classes/CSharpFundamentals/Enumeration.cs
@@ -0,0 +1,49 @@
+using System;
+public class Person
+{
+    string name;
+
+      public void SetName(string value)
+    {
+        // if(value.Length > 2)
+        // {
+        //     name = value;
+        // }
+    }
+
+    // full property syntax
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+
+        set
+        {
+        if(value.Length > 2)
+            name = value;
+        
+        }
+    }
+    // DateTime dob; 
+    public DateTime Dob {get; set; } // private field auto implemented property
+
+    // public string address;
+    
+    // Readonly property
+    public string Address{get;}
+
+    public char Gender{get; private set; } = 'F';
+
+    // Default Value
+    public Gender Sex {get; private set; } = Gender.Female;
+    public enum Gender
+    {
+        Male,
+        Fe,
+        Others
+    }
+
+    // Term --> Strongly typed and weakly typed
+}
